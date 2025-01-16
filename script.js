33294b512875328cec53ad024356d3705f5b2ed4
CAVOK21 <d2019999@gmail.com>
2025-01-16 23:53:09 +0300

    add static

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const map = L.map('map').setView([53.992666, 27.320273], 13);
 const modal = document.getElementById('modal');
 const closeButton = document.getElementById('close');
 const image = document.getElementById('image');
+const title = document.getElementById('title');
 
 closeButton.addEventListener('click', onModalClose);
 
@@ -10,10 +11,11 @@ const OpenStreetMap_BZH = L.tileLayer('https://tile.openstreetmap.bzh/br/{z}/{x}
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="http://www.openstreetmap.bzh/" target="_blank">Breton OpenStreetMap Team</a>',
 }).addTo(map);
 
-function onMarkerClick(src) {
+function onMarkerClick(config) {
   return function (e) {
     modal.classList.add('open');
-    image.src = src;
+    image.src = config.src;
+    title.innerText = config.label;
   }
 }
 
@@ -29,24 +31,47 @@ const configs = [
   },
   {
     coords: [54.03260355694114, 27.254911425654054],
-    label: "Крыница пясчанка",
+    label: "Крыница пяшчанка",
+    src: "/static/paschanka.jpg",
+  },
+  {
+    coords: [54.03599982506858, 27.248248818462006],
+    label: "Хмелевский водопад",
     src: "/static/grebkanal.jpg",
-  }
+  },
+  {
+    coords: [53.99998865610981, 27.31162980368535],
+    label: "Гребной канал",
+    src: "/static/grebkanal2.jpg",
+  },
+  {
+    coords: [53.976312476392536, 27.3405276701716],
+    label: "Санаторий юность",
+    src: "/static/unost.jpg",
+  },
+  {
+    coords: [53.97302509487277, 27.344890739131404],
+    label: "Остров любви",
+    src: "/static/lubov.jpg",
+  },
 ];
 
 const markers = configs.map(
   config => L.marker(config.coords)
     .addTo(map)
     .bindTooltip(config.label, { permanent: true, direction: 'right' })
-    .on('click', onMarkerClick(config.src))
+    .on('click', onMarkerClick(config))
 );
 
-const polygon = L.polygon([
+const polygon1 = L.polygon([
   [54.00140725853155, 27.3111317697426],
   [54.00058233994049, 27.309039325319457],
   [53.98515936618216, 27.328621227237424],
   [53.98596958645076, 27.330560565971066]
 ]).addTo(map).bindPopup("Место для сплава")
 
-
+const polygon2 = L.polygon([
+  [53.99970895963, 27.30371626223849],
+  [53.997278611408966, 27.30515009794397]
+]).addTo(map).bindPopup("Отличное место для рыбалки с удобным берегом")
 
